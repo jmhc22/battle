e6f026c1d4fc33addd6a95eea327af16a85375a4
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-10-02 15:10:16 +0100

    Switching turns affects attack confirmation message

--- app.rb
+++ app.rb
@@ -19,8 +19,12 @@ class Battle < Sinatra::Base
   get '/attack' do
     @game = $game
     @game.attack(@game.player_2)
-    @game.switch_turns
     erb :attack
+  end
+
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect '/play'
   end
 
   get '/play' do
--- spec/features/switching_turns_spec.rb
+++ spec/features/switching_turns_spec.rb
@@ -10,7 +10,7 @@ feature 'turns tracking' do
       scenario 'after player 1 attacks' do
         sign_in_and_play
         click_button 'Attack'
-        click_link 'OK'
+        click_button 'OK'
         expect(page).not_to have_content "James's turn"
         expect(page).to have_content "Sid's turn"
       end
