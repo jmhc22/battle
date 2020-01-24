8194347356ebed433603a3c54a0118978f56d342
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2020-01-24 16:18:01 +0000

    Attack animation appears when selecting attack

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,14 +1,25 @@
 $(document).ready( () => {
   console.log('loaded')
 
+
+
+
   $.get('/api/battle', (data) => {
     console.log(data)
   })
 
   $('.attack-button').click(() => {
     const move = event.target.id
+    attackAnimation(move)
     $.post('/api/battle', `{ "attack": "${move}" }`, (data) => {
       console.log(data)
     })
   })
+
+  const attackAnimation = (move) => {
+    $('#attack-space').html(`<img id="attack-img" src="images/p1-${move}-attack.png">`)
+    setTimeout(() => {
+      $('#attack-img').fadeOut('slow')
+    }, 500);
+  }
 })
